845efd0d017fe6b87de4e944ad22f1b2d322e4ae
Utkin Artem <solid.utkin@gmail.com>
2020-07-23 23:43:08 +0300

    update main.go

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/SolidShake/wetherboy-tg-bot/iternal/config"
-	"github.com/SolidShake/wetherboy-tg-bot/iternal/types"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	_ "go.mongodb.org/mongo-driver/bson"
-	_ "go.mongodb.org/mongo-driver/mongo/readpref"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"reflect"
-)
 
-import "github.com/SolidShake/wetherboy-tg-bot/iternal/connections"
+	"github.com/SolidShake/wetherboy-tg-bot/iternal/config"
+	"github.com/SolidShake/wetherboy-tg-bot/iternal/connections"
+	"github.com/SolidShake/wetherboy-tg-bot/iternal/types"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	_ "go.mongodb.org/mongo-driver/bson"
+	_ "go.mongodb.org/mongo-driver/mongo/readpref"
+)
 
 func main() {
 	dbConnection := connections.MongoConnection{}
@@ -50,24 +50,22 @@ func main() {
 		}
 
 		subButton := tgbotapi.KeyboardButton{
-			Text: "Подписаться на прогноз",
+			RequestLocation: true,
+			Text:            "Подписаться на прогноз",
 		}
 
 		//unsubButton := tgbotapi.KeyboardButton{
 		//	Text: "Подписаться на прогноз",
 		//}
 
-		var newChatMembers = update.Message.Chat.ID
-		if newChatMembers != 0 {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Для получения актуальной погоды нажмите кнопку ниже")
-			msg.ReplyMarkup = tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{btn})
-			bot.Send(msg)
-		}
-
 		if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
 			switch update.Message.Text {
 			case "Подписаться на прогноз":
 				dbConnection.AddSub(update.Message.Chat.ID, *update.Message.Location)
+			default:
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Для получения актуальной погоды нажмите кнопку ниже")
+				msg.ReplyMarkup = tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{btn})
+				bot.Send(msg)
 			}
 		}
 
@@ -96,10 +94,6 @@ func getWeatherInfoByCoord(latitude, longitude float64) string {
 	if err != nil {
 		log.Println(err)
 	}
-
-	// log.Println("===========")
-	// log.Println(string(body))
-	// log.Println("=========")
 
 	result := fmt.Sprintf(
 		"Доброго времени суток!\n\nВы находитесь в городе %s\nТемпература: %.2f ℃\nОщущается как %.2f ℃\nОжидаемая погода: %s\nСкорость ветра: %.2f м/с",
